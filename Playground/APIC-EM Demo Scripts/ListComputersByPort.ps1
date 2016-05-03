e77f71f2313200b8c06086557f2050de1e80a338
darrenstarr <darren@nocturnal.no>
2016-05-03 15:54:10 +0200

    Added initial versions of PowerShell APIC-EM Scripts

---
+++ Playground/APIC-EM Demo Scripts/ListComputersByPort.ps1
@@ -0,0 +1,70 @@
+﻿$apicEMServer = "10.209.31.103"
+$apicEMUsername = "admin"
+$apicEMPassword = "C1sco12345"
+
+#----------------------------------------------------------------------------
+$ticketURL = "https://{0}/api/v1/ticket" -f $apicEMServer
+$ticketRequest = "{ `"username`": `"" + $apicEMUsername + "`", `"password`": `"" + $apicEMPassword +"`" }" 
+
+
+$getHostURL = "https://" + $apicEMServer + "/api/v1/host?hostIp={0}" 
+$getNetworkDeviceURL = "https://" + $apicEMServer + "/api/v1/network-device/{0}" 
+
+function Get-IPAddress {
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$True)]
+        [ValidateNotNullOrEmpty()]
+        [string]$HostName
+    )
+    Try {
+        $a = [System.Net.Dns]::GetHostEntry($HostName) 
+    } Catch {
+        return $null
+    }
+
+    If ($a) {
+        ($IPs = $a.AddressList | Foreach {$_.IPAddressToString}) | Where { -Not $_.Contains(":") } | sort @{Expression={[Version]$_}} -unique 
+    } Else {$null}
+}
+
+$Computers =  Get-ADComputer  -Filter {(enabled -eq "true") } 
+
+$result = Invoke-RestMethod -Uri $ticketURL  -Body $ticketRequest -Method Post -ContentType "application/json"
+$ticket = $result.response.serviceTicket
+
+ForEach ($computer in $Computers) {
+    $addresses = Get-IPAddress -HostName $computer.DNSHostName
+    If (!$addresses -or $address.Length -eq 0) {
+        Continue
+    }
+
+    Write-Host ("Computer : {0} ({1})" -f $computer.Name, $computer.DNSHostName)
+    ForEach ($address in $addresses) {
+        If($address.Contains(":")) {
+            Continue
+        }
+        
+        $getHostResult = Invoke-RestMethod -Uri ($getHostURL -f $address) -Header @{"X-Auth-Token" = $ticket}  -ContentType "application/json"
+        If($getHostResult.Response.Count -gt 0) {
+            ForEach ($hostDevice in $getHostResult) {
+                If ($hostDevice.response.Count -eq 0) {
+                    continue
+                }
+
+                $getNetworkDeviceResult = Invoke-RestMethod -Uri ($getNetworkDeviceURL -f $hostDevice.response[0].connectedNetworkDeviceId) -Header @{"X-Auth-Token" = $ticket}  -ContentType "application/json"
+
+                If (!$getNetworkDeviceResult -or $getNetworkDeviceResult.response.Count -eq 0) {
+                    $networkDeviceName = "<unknown>"
+                } Else {
+                    $networkDeviceName = $getNetworkDeviceResult.response[0].hostName
+                }
+
+                Write-Host ("IP Address : {0} is located on {1} and connected to interface {2}" -f $address, $networkDeviceName, $hostDevice.connectedInterfaceName)
+            }
+        } Else {
+            Write-Host ("   IP - {0} not found" -f $address)
+        }
+    }
+}
+
---
+++ Playground/APIC-EM Demo Scripts/RESTapiCall.ps1
@@ -0,0 +1,27 @@
+﻿add-type @"
+    using System.Net;
+    using System.Security.Cryptography.X509Certificates;
+    public class TrustAllCertsPolicy : ICertificatePolicy {
+        public bool CheckValidationResult(
+            ServicePoint srvPoint, X509Certificate certificate,
+            WebRequest request, int certificateProblem) {
+            return true;
+        }
+    }
+"@
+[System.Net.ServicePointManager]::CertificatePolicy = New-Object TrustAllCertsPolicy
+
+$ipToFind = "10.205.265.2"
+
+$result = Invoke-RestMethod -Uri "https://10.209.31.103/api/v1/ticket"  -Body '{ "username": "admin", "password": "C1sco12345" }' -Method Post -ContentType "application/json"
+
+$ticket = $result.response.serviceTicket
+
+$result = Invoke-RestMethod -Uri ("https://10.209.31.103/api/v1/host?hostIp={0}" -f $ipToFind) -Header @{"X-Auth-Token" = $ticket}  -ContentType "application/json"
+
+$response = $result.response
+
+$result = Invoke-RestMethod -Uri ("https://10.209.31.103/api/v1/network-device/{0}" -f $response.connectedNetworkDeviceId) -Header @{"X-Auth-Token" = $ticket}  -ContentType "application/json"
+
+
+Write-Host ("IP Address : {0} is located on {1} and connected to interface {2}" -f $ipToFind, $result.response[0].hostName, $response.connectedInterfaceName)
